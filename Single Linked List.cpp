18f499511999c3474c86ba92ef02c388f3547b47
mufid1012 <mufidghibran1012@gmail.com>
2024-04-25 14:11:48 +0700

    membuat cout & cin Masukan Nim & Nama

--- Single Linked List.cpp
+++ Single Linked List.cpp
@@ -13,4 +13,8 @@ void addNode() {
     int nim;
     string nama;
     Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
 }
